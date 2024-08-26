7a4995e66319e1d597b56dc8337f6d5d9690e83a
Ahmed Taha <atbasyoni98@gmail.com>
2024-08-26 16:41:37 +0300

    Remove Unnecessary Files

--- HotelReservationApi/obj/Debug/net8.0/HotelReservationApi.AssemblyInfo.cs
+++ HotelReservationApi/obj/Debug/net8.0/HotelReservationApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9f42d471e9b2b8a8882b0b77b7be5dac4c87ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83351ca89e5606e2615bb60f2bf7006044890b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
